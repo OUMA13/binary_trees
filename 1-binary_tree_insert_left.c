dda52c8c00c298245d7a41908858c545ed2503d6
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-03-05 03:14:34 -0800

    binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,8 +14,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nw_no;
 
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
+
 	nw_no= binary_tree_node(parent, value);
 	if (nw_no == NULL)
 		return (NULL);
